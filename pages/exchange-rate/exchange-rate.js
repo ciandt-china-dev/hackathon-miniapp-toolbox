2a304239f846675595f2bccbd748a33e80d95f23
Steven Lin <stevenl@ciandt.com>
2016-12-17 21:01:16 +0800

    exchange rate code

--- pages/exchange-rate/exchange-rate.js
+++ pages/exchange-rate/exchange-rate.js
@@ -6,10 +6,12 @@ var sign = "9437958a31ca08fba8ff87013d7ab7ba";
 Page({
   data: {
     usdToCny: "",
-    // cnyToUsd: "",
     country: [],
     fromIndex: '',
     toIndex: '',
+    fromMoney: 1,
+    toMoney: '',
+    notice:'',
   },
   onLoad: function (options) {
     console.log('initCountry');
@@ -17,22 +19,52 @@ Page({
   },
   onReady: function () {
     console.log('onready');
+    var from = wx.getStorageSync('default_ex_rate_from');
+    if (from == "") {
+      from = "USD 美元";
+      wx.setStorageSync('default_ex_rate_from', from);
+    }
+    var to = wx.getStorageSync('default_ex_rate_to');
+    if (to == "") {
+      to = "CNY 人民币";
+      wx.setStorageSync('default_ex_rate_to', to);
+    }
     var _t = this;
     wx.getStorage({
       key: 'country',
       success: function (res) {
         _t.setData({
           country: res.data,
-          fromIndex: res.data.indexOf("USD 美元"),
-          toIndex: res.data.indexOf("CNY 人民币"),
+          fromIndex: res.data.indexOf(from),
+          toIndex: res.data.indexOf(to),
         });
       }
-    })
-    // this.setData({
-    //   country: res
-    //   usdToCny: 6.28320,
-    //   cnyToUsd: 0.38239,
-    // });
+    });
+    wx.request({
+      url: appurl,
+      data: {
+        'app': 'finance.rate',
+        'scur': from.substring(0, 3),
+        'tcur': to.substring(0, 3),
+        'appkey': appkey,
+        'sign': sign,
+        'format': 'json',
+      },
+      method: "GET",
+      success: function (res) {
+        var data = res.data;
+        if (data.success == "1") {
+          var rate = data.result.rate;
+          var update = data.result.update;
+          _t.setData({
+            usdToCny: rate.toFixed(6),
+            notice:'数据仅供参考，更新：' + update,
+          });
+        }
+      },
+      fail: function () { },
+      complete: function () {}
+    });
   },
   initCountry: function (cb) {
     var _t = this;
@@ -65,14 +97,23 @@ Page({
     });
 
   },
-  getRate: function (e) {
-    console.log('test');
+  formSubmit: function (e) {
+    var fromIndex = e.detail.value.from_country;
+    var toIndex = e.detail.value.to_country;
+    var fromMoney = e.detail.value.from_money;
+    var country = wx.getStorageSync('country');
+    var _t = this;
+    wx.showToast({
+      'title' : '加载中',
+      icon: 'loading',
+      duration: 2000,
+    });
     wx.request({
       url: appurl,
       data: {
         'app': 'finance.rate',
-        'scur': e.detail.value.from_country,
-        'tcur': e.detail.value.to_country,
+        'scur': country[fromIndex].substring(0, 3),
+        'tcur': country[toIndex].substring(0, 3),
         'appkey': appkey,
         'sign': sign,
         'format': 'json',
@@ -80,15 +121,50 @@ Page({
       method: "GET",
       success: function (res) {
         var data = res.data;
-        if (data.success) {
-
+        console.log(data);
+        if (data.success == "1") {
+          var rate = data.result.rate;
+          var update = data.result.update;
+          _t.setData({
+            toMoney: (rate * fromMoney).toFixed(6),
+            notice:'数据仅供参考，更新：' + update,
+          });
         }
       },
-      fail: function () { }
+      fail: function () { },
+      complete: function () {
+        wx.hideToast();
+      }
     })
   },
   switchCountry: function (e) {
-    console.log('switch');
+    var from = wx.getStorageSync('default_ex_rate_from');
+    var to = wx.getStorageSync('default_ex_rate_to');
+    var _t = this;
+    wx.getStorage({
+      key: 'country',
+      success: function (res) {
+        _t.setData({
+          fromIndex: res.data.indexOf(to),
+          toIndex: res.data.indexOf(from),
+        })
+        wx.setStorageSync('default_ex_rate_from', to);
+        wx.setStorageSync('default_ex_rate_to', from);
+      }
+    })
+  },
+  bindFromCountryChange: function (e) {
+    this.setData({
+      fromIndex: e.detail.value
+    });
+    var country = wx.getStorageSync('country');
+    wx.setStorageSync('default_ex_rate_from', country[e.detail.value]);
+  },
+  bindToCountryChange: function (e) {
+    this.setData({
+      toIndex: e.detail.value
+    })
+    var country = wx.getStorageSync('country');
+    wx.setStorageSync('default_ex_rate_to', country[e.detail.value]);
   }
-
 })
